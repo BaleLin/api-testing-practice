7bafeef7279b03cee3817fb99a88af97cb372aac
BaleLin <bale.lin@oocl.com>
2018-07-30 20:49:02 +0800

    test speed records have been set by cars from the UK

--- src/test/java/exercises/RestAssuredExercises5Test.java
+++ src/test/java/exercises/RestAssuredExercises5Test.java
@@ -70,7 +70,9 @@ public class RestAssuredExercises5Test {
 		given().
 			spec(requestSpec).
 		when().
-		then();
+		get("/xml/speedrecords").
+		then().
+		body("speedRecords.car[5].@country",equalTo("UK"));
 	}
 	
 	/*******************************************************
